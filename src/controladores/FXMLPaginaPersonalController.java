ef482dc2610a87cbc8acf0a6b4c58db4b157d4fd
IkZeta <114521397+IkZeta@users.noreply.github.com>
2023-05-28 11:49:37 +0200

    Add files via upload

--- src/controladores/FXMLPaginaPersonalController.java
+++ src/controladores/FXMLPaginaPersonalController.java
@@ -5,15 +5,21 @@
 package controladores;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -33,9 +39,9 @@ public class FXMLPaginaPersonalController implements Initializable {
     @FXML
     private MFXButton anulButton;
     @FXML
-    private Label verDispButton;
+    private MFXButton verDispButton;
     @FXML
-    private Label ActButton;
+    private MFXButton actButton;
 
     /**
      * Initializes the controller class.
@@ -46,24 +52,69 @@ public class FXMLPaginaPersonalController implements Initializable {
     }    
 
     @FXML
-    private void clickReserv(ActionEvent event) {
-        
+    private void clickReserv(ActionEvent event)throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/FXMLPistas.fxml"));
+                Parent root = loader.load();
+                
+                
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("Pistas");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                reservButton.getScene().getWindow().hide();
     }
 
     @FXML
-    private void clickVerRes(ActionEvent event) {
+    private void clickVerRes(ActionEvent event)throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/MisReservas.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("MisReservas");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                verResButton.getScene().getWindow().hide(); 
     }
 
     @FXML
-    private void clickAnul(ActionEvent event) {
+    private void clickAnul(ActionEvent event)throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/MisReservas.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("AnularReservas");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                anulButton.getScene().getWindow().hide();
+    }
+    @FXML
+    private void clickVerDisp(ActionEvent event)throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/Reservas.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("Reservas");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                verDispButton.getScene().getWindow().hide();
     }
 
     @FXML
-    private void clickVerDisp(MouseEvent event) {
-    }
-
-    @FXML
-    private void clickAct(MouseEvent event) {
+    private void clickAct(ActionEvent event)throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/FXMLRegistro.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("Registro");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                actButton.getScene().getWindow().hide();
     }
     
 }
