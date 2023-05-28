a28ccd5c73765a218820fb1637799b613da2da73
IkZeta <114521397+IkZeta@users.noreply.github.com>
2023-05-28 13:26:52 +0200

    Add files via upload

--- src/controladores/MisReservas.java
+++ src/controladores/MisReservas.java
@@ -14,12 +14,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import model.Booking;
 import model.Club;
@@ -66,7 +71,16 @@ public class MisReservas implements Initializable {
     }    
 
     @FXML
-    private void clickVolver(ActionEvent event) {
+    private void clickVolver(ActionEvent event)  throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/vistas/FXMLPaginaPersonal.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle("PaginaPersonal");
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.show();
+                botonVolver.getScene().getWindow().hide();
     }
 
 
