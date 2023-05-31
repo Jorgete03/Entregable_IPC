1cb9cf7ed0a43e11338ce5680d658c7717395301
Jorgete03 <132222291+Jorgete03@users.noreply.github.com>
2023-05-31 20:19:58 +0200

    Add files via upload

--- src/controladores/Reservas.java
+++ src/controladores/Reservas.java
@@ -68,9 +68,8 @@ public class Reservas implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
         try {
-            if(id==2){
             nickName.setText(member.getNickName());
-             fotoPerfil.setImage(member.getImage());}
+             fotoPerfil.setImage(member.getImage());
             club = Club.getInstance();
         } catch (IOException | ClubDAOException ex) {
             Logger.getLogger(Reservas.class.getName()).log(Level.SEVERE, null, ex);
@@ -113,6 +112,7 @@ public class Reservas implements Initializable {
 
     @FXML
     private void clickBuscar(ActionEvent event) {
+        if (member.checkLogin(nombreUsuario.getText())){
         String name = nombreUsuario.getText();
     for (int i = currentIndex + 1; i < listView1.getItems().size(); i++) {
         Booking booking = listView1.getItems().get(i);
@@ -124,6 +124,7 @@ public class Reservas implements Initializable {
             
             break;
         }
+    }
     }
 
     }
